1e67a41ee8e58cafe5ae84b17780b610dea3427f
unknown <verma.shubham2537@gmail.com>
2023-02-28 18:05:09 +0530

    Five commit

--- src/main/java/com/driver/WhatsappService.java
+++ src/main/java/com/driver/WhatsappService.java
@@ -6,32 +6,29 @@ import java.util.List;
 
 @Service
 public class WhatsappService {
-    WhatsappRepository whatsappRepository = new WhatsappRepository();
-
-    public boolean isNewUser(String mobile){
+    WhatsappRepository whatsappRepository = new WhatsappRepository() ;
+    public boolean isNewUser(String mobile) {
         return whatsappRepository.isNewUser(mobile);
     }
 
-    public String createUser(String name, String mobile){
+    public String createUser(String name, String mobile) {
         whatsappRepository.createUser(name, mobile);
-        return "Success";
+        return "SUCCESS";
     }
 
-    public Group createGroup(List<User> users){
+    public Group createGroup(List<User> users) {
         return whatsappRepository.createGroup(users);
     }
-    public int createMessage(String content){
+
+    public int createMessage(String content) {
         return whatsappRepository.createMessage(content);
     }
+
     public int sendMessage(Message message, User sender, Group group) throws Exception {
         return whatsappRepository.sendMessage(message, sender, group);
     }
-    public String changeAdmin(User approver, User user, Group group) throws Exception{
-        //Throw "Group does not exist" if the mentioned group does not exist
-        //Throw "Approver does not have rights" if the approver is not the current admin of the group
-        //Throw "User is not a participant" if the user is not a part of the group
-        //Change the admin of the group to "user" and return "SUCCESS". Note that at one time there is only one admin and the admin rights are transferred from approver to user.
 
+    public String changeAdmin(User approver, User user, Group group) throws Exception {
         return whatsappRepository.changeAdmin(approver, user, group);
     }
 }
